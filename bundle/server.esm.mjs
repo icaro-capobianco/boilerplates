292b1f9530bcb48de1028dff75c51442a217c016
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2022-07-06 10:55:15 -0300

    Keep bundle away from the repo

--- bundle/server.esm.mjs
+++
@@ -1,9 +0,0 @@
-import Koa from 'koa';
-import serve from 'koa-static';
-import path from 'path';
-
-const app = new Koa();
-const cwd = process.cwd();
-const dist = path.join(cwd, '/build/dist/');
-app.use(serve(dist));
-app.listen(3000, () => console.log('https://localhost:3000'));
